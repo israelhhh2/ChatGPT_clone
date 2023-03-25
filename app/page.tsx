cff87722d7a662901bb71f0f822afeba5d88858c
Israel Hernandez <israelhhh2@gmail.com>
2023-03-24 23:28:45 -0700

    fixed eslint errors by double quotes and single quotes to their escape keys

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,23 @@ async function HomePage() {
       <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
 
       <div className="flex space-x-2 text-center">
-        <div className="">
+        <div>
           <div className="flex flex-col items-center justify-center mb-5">
             <SunIcon className="h-8 w-8" />
             <h2>Examples</h2>
           </div>
 
           <div className="space-y-2">
-            <p className="infoText">"Explain Something to me"</p>
+            <p className="infoText">&ldquo;Explain Something to me&rdquo;</p>
             <p className="infoText">
-              "What is the difference between a dog and a cat?"
+              &ldquo;What is the difference between a dog and a cat?&rdquo;
             </p>
-            <p className="infoText">"What is the color of the sun?"</p>
+            <p className="infoText">
+              &ldquo;What is the color of the sun?&rdquo;
+            </p>
           </div>
         </div>
-        <div className="">
+        <div>
           <div className="flex flex-col items-center justify-center mb-5">
             <BoltIcon className="h-8 w-8" />
             <h2>Capabilities</h2>
@@ -33,14 +35,14 @@ async function HomePage() {
           <div className="space-y-2">
             <p className="infoText">Change the ChatGPT Model to use</p>
             <p className="infoText">
-              Messages are stored in Firebase's Firestore
+              Messages are stored in Firebase&apos;s Firestore
             </p>
             <p className="infoText">
               Hot Toast notifications when ChatGPT is thinking!
             </p>
           </div>
         </div>
-        <div className="">
+        <div>
           <div className="flex flex-col items-center justify-center mb-5">
             <ExclamationTriangleIcon className="h-8 w-8" />
             <h2>Limitations</h2>
